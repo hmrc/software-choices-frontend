2fd27e5f7865102976ab4fecf9f2eb85b80934db
Ladon Jackson <ladon.jackson@digital.hmrc.gov.uk>
2019-01-11 12:25:48 +0000

    added ip whitelisting

--- app/uk/gov/hmrc/softwarechoicesfrontend/config/WhitelistFilter.scala
+++ app/uk/gov/hmrc/softwarechoicesfrontend/config/WhitelistFilter.scala
@@ -1,5 +1,24 @@
 package uk.gov.hmrc.softwarechoicesfrontend.config
 
-class WhitelistFilter {
+import akka.stream.Materializer
+import javax.inject.Inject
+import play.api.mvc.{Call, RequestHeader, Result}
+import uk.gov.hmrc.whitelist.AkamaiWhitelistFilter
 
+import scala.concurrent.Future
+
+
+class WhitelistFilter @Inject()(val appConfig: AppConfig, implicit val mat: Materializer) extends AkamaiWhitelistFilter {
+
+  override val whitelist: Seq[String] = appConfig.whitelistIps
+  override val destination: Call = Call("GET", appConfig.shutterPage)
+  override val excludedPaths: Seq[Call] = appConfig.whitelistExcludedPaths
+
+  override def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
+    if(appConfig.whiteListEnabled) {
+      super.apply(f)(rh)
+    } else {
+      f(rh)
+    }
+  }
 }
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-
+    "uk.gov.hmrc"             %% "play-whitelist-filter"    % "1.1.0",
     "uk.gov.hmrc"             %% "govuk-template"           % "5.26.0-play-25",
     "uk.gov.hmrc"             %% "play-ui"                  % "7.27.0-play-25",
     "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.6.0"
