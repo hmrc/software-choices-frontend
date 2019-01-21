30e476e6377ad157ed6f512c2f5dc3ce74c54fbd
MJCallahanPage <matthew.page@digital.hmrc.gov.uk>
2019-01-21 11:42:29 +0000

    [MSCC-44] Added test for error templae

---
+++ test/views/templates/ErrorTemplateViewSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.templates
+
+import org.jsoup.Jsoup
+import play.twirl.api.Html
+import utils.TestUtils
+
+class ErrorTemplateViewSpec extends TestUtils {
+
+  object Selectors {
+    val heading = "h1"
+    val content = "#content p"
+  }
+
+  "The Error Template" when {
+
+    "given a title and some Html content" should {
+
+      lazy val view = views.html.templates.error_template("A Title", "A Heading", "Some Text")
+      lazy val document = Jsoup.parse(view.body)
+
+      "have the correct title" in {
+        document.title shouldBe "A Title"
+      }
+
+      "have the correct heading" in {
+        document.select(Selectors.heading).text shouldBe "A Heading"
+      }
+
+      "have the correct content" in {
+        document.select(Selectors.content).text shouldBe "Some Text"
+      }
+    }
+  }
+}
