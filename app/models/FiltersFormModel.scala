4e003372c17aea72994178249865dfd00d2f874c
callum.cooper <callum.cooper@digital.hmrc.gov.uk>
2019-02-19 16:55:50 +0000

    [MSCC-115] - Filter form and model

---
+++ app/models/FiltersFormModel.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import enums.Filter
+import enums.Filter._
+
+case class FiltersFormModel(filters: Seq[Filter.Value],
+                            searchTerm: Option[String] = None)
+
+object FiltersFormModel {
+
+  def customApply(searchTerm: Option[String] = None,
+            business: Option[Filter.Value] = None,
+            agent: Option[Filter.Value] = None,
+            viewReturn: Option[Filter.Value] = None,
+            viewLiabilities: Option[Filter.Value] = None,
+            viewPayments: Option[Filter.Value] = None,
+            accounting: Option[Filter.Value] = None,
+            spreadsheets: Option[Filter.Value] = None): FiltersFormModel = {
+
+    val filters: Seq[Filter.Value] = Seq(business, agent, viewReturn, viewLiabilities, viewPayments, accounting, spreadsheets).flatten
+
+    new FiltersFormModel(filters, searchTerm)
+  }
+
+  def customUnapply(arg: FiltersFormModel): Option[(Option[String],
+    Option[Filter.Value],
+    Option[Filter.Value],
+    Option[Filter.Value],
+    Option[Filter.Value],
+    Option[Filter.Value],
+    Option[Filter.Value],
+    Option[Filter.Value])] = {
+
+    def giveBack(filter: Filter.Value): Option[Filter.Value] = if (arg.filters.contains(filter)) Some(filter) else None
+
+    Some((
+      arg.searchTerm,
+      giveBack(BUSINESS),
+      giveBack(AGENT),
+      giveBack(VIEW_RETURN),
+      giveBack(VIEW_LIABILITIES),
+      giveBack(VIEW_PAYMENTS),
+      giveBack(ACCOUNTING),
+      giveBack(SPREADSHEETS)
+    ))
+  }
+}
---
+++ test/models/FiltersFormModelSpec.scala
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import utils.TestUtils
+import enums.Filter._
+
+class FiltersFormModelSpec extends TestUtils {
+
+  "FiltersFormModel" should {
+    "contain a list of filters" in {
+
+      val actualResult = FiltersFormModel(Seq(AGENT, BUSINESS)).filters
+      val expectedResult = Seq(AGENT, BUSINESS)
+
+      actualResult shouldBe expectedResult
+    }
+
+    "contain a search term when defined" in {
+
+      val actualResult = FiltersFormModel(Seq.empty, Some("Abratax")).searchTerm
+      val expectedResult = Some("Abratax")
+
+      actualResult shouldBe expectedResult
+    }
+  }
+
+  "FiltersModel.customApply" when {
+
+    "all filters are applied" should {
+
+      "create a model with a list of all filters" in {
+
+        val actualResult = FiltersFormModel.customApply(
+          Some("term"),
+          Some(BUSINESS),
+          Some(AGENT),
+          Some(VIEW_RETURN),
+          Some(VIEW_LIABILITIES),
+          Some(VIEW_PAYMENTS),
+          Some(ACCOUNTING),
+          Some(SPREADSHEETS)
+        ).filters
+
+        val expectedResult = Seq(BUSINESS, AGENT, VIEW_RETURN, VIEW_LIABILITIES, VIEW_PAYMENTS, ACCOUNTING, SPREADSHEETS)
+
+        actualResult shouldBe expectedResult
+
+      }
+    }
+
+    "Agent, View Return and Accounting filters are applied" should {
+
+      "create a model with Agent, View Return and Accounting" in {
+
+        val actualResult = FiltersFormModel.customApply(
+          agent = Some(AGENT),
+          viewReturn = Some(VIEW_RETURN),
+          accounting = Some(ACCOUNTING)
+        ).filters
+
+        val expectedResult = Seq(AGENT, VIEW_RETURN, ACCOUNTING)
+
+        actualResult shouldBe expectedResult
+
+      }
+    }
+
+
+    "No filters are applied" should {
+
+      "create a model with Agent, View Return and Accounting" in {
+        FiltersFormModel.customApply().filters shouldBe Seq()
+      }
+    }
+  }
+
+  "FiltersModel.customUnapply" when {
+
+    "all filters are applied" should {
+
+      "return all values" in {
+
+        val actualResult = FiltersFormModel.customUnapply(FiltersFormModel(
+          Seq(BUSINESS, AGENT, VIEW_RETURN, VIEW_LIABILITIES, VIEW_PAYMENTS, ACCOUNTING, SPREADSHEETS),
+          Some("term")
+        ))
+
+        val expectedResult = Some((
+          Some("term"),
+          Some(BUSINESS),
+          Some(AGENT),
+          Some(VIEW_RETURN),
+          Some(VIEW_LIABILITIES),
+          Some(VIEW_PAYMENTS),
+          Some(ACCOUNTING),
+          Some(SPREADSHEETS)
+        ))
+
+        actualResult shouldBe expectedResult
+
+      }
+    }
+
+    "Agent, View Return and Accounting filters are applied" should {
+
+      "return Agent, View Return and Accounting values" in {
+
+        val actualResult = FiltersFormModel.customUnapply(FiltersFormModel(
+          Seq(AGENT, VIEW_RETURN, ACCOUNTING)
+        ))
+
+        val expectedResult = Some((None, None, Some(AGENT), Some(VIEW_RETURN), None, None, Some(ACCOUNTING), None))
+
+        actualResult shouldBe expectedResult
+
+      }
+    }
+
+
+    "No filters are applied" should {
+
+      "return no values" in {
+        FiltersFormModel.customUnapply(FiltersFormModel(Seq())) shouldBe Some((None, None, None, None, None, None, None, None))
+      }
+    }
+  }
+
+}
