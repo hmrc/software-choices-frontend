e24b4801b60d4da968bcffc0ba5efeac1b35ede2
Stephen Ward <15212062+stephenkward@users.noreply.github.com>
2019-07-09 10:50:16 +0100

    SAR-4077 - Dep Uplift

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,11 +9,11 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "play-whitelist-filter"    % "2.0.0",
     "uk.gov.hmrc"             %% "govuk-template"           % "5.27.0-play-25",
     "uk.gov.hmrc"             %% "play-ui"                  % "7.40.0-play-25",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.12.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.13.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "hmrctest"                    % "3.8.0-play-25"         % "test",
+    "uk.gov.hmrc"             %% "hmrctest"                    % "3.9.0-play-25"         % "test",
     "org.scalatest"           %% "scalatest"                   % "3.0.5"                 % "test",
     "org.jsoup"               %  "jsoup"                       % "1.11.3"                % "test",
     "com.typesafe.play"       %% "play-test"                   % current                 % "test",
