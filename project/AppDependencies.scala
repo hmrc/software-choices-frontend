2c7397d8f1090d88cfc5003dff7c0cfb760f99f5
Daveo-G <Daveo-G@users.noreply.github.com>
2019-08-29 11:40:22 +0100

    SAR-4465 - Updates to software list 28-08-2019

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "play-whitelist-filter"    % "2.0.0",
     "uk.gov.hmrc"             %% "govuk-template"           % "5.27.0-play-25",
     "uk.gov.hmrc"             %% "play-ui"                  % "7.40.0-play-25",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.13.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.15.0"
   )
 
   val test = Seq(
