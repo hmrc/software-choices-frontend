ad21f9755ad57d3d6c9bf41948b7c6a6f8ed5efc
Alex Rimmer <31283208+AlexRimmerHMRC@users.noreply.github.com>
2019-09-17 16:46:29 +0100

    SAR-4467 - Added accessibility statement page

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "play-whitelist-filter"    % "2.0.0",
     "uk.gov.hmrc"             %% "govuk-template"           % "5.27.0-play-25",
-    "uk.gov.hmrc"             %% "play-ui"                  % "7.40.0-play-25",
+    "uk.gov.hmrc"             %% "play-ui"                  % "8.0.0-play-25",
     "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.15.0"
   )
 
