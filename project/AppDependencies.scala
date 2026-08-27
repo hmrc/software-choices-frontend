0366eb3c9f1ff25039b242cb9d5054fee4fa3b78
arunankannan <103437165+arunankannan@users.noreply.github.com>
2026-08-27 18:23:59 +0100

    DL-20181 VAT Software choices - Product Update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -26,7 +26,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %%  "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %%  "play-frontend-hmrc-play-30" % "13.9.0",
+    "uk.gov.hmrc" %%  "play-frontend-hmrc-play-30" % "13.11.0",
     "org.webjars" %   "jquery"                     % "4.0.0"
   )
 
